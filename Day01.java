8879b32eaa074adbd1bb905f12bc40bda385d08c
Sheva <sheva.sepot@gmail.com>
2025-01-07 08:40:48 +0700

    first commit

---
+++ Day01.java
@@ -0,0 +1,39 @@
+public class Day01 {
+
+    public byte byteNumber = 1;
+
+    public static void main(String[] args) {
+
+        System.out.println("Test");
+
+        byte tinyNumber = 1;
+        short smallNumber = 1;
+        int mediumNumber = 1;
+        long largeNumber = 1;
+
+        float mediumDecimal = 1.5f;
+        double largeDecimal = 1.5;
+
+        System.out.println(tinyNumber);
+        System.out.println(smallNumber);
+        System.out.println(mediumNumber);
+        System.out.println(largeNumber);
+
+        System.out.println(mediumDecimal);
+        System.out.println(largeDecimal);
+
+        boolean truthy = true;
+        boolean falsy = false;
+
+        System.out.println(truthy);
+        System.out.println(falsy);
+
+        Integer mediumObjectNumber = 5;
+
+        String word = "sheva";
+        System.out.println(word.charAt(4));
+
+        
+    }
+    
+}
