bc573a60b48812a4c62cc252eed81e4683faee23
Sheva <sheva.sepot@gmail.com>
2025-01-13 21:06:33 +0700

    feat:pembekalan day 06 update

--- Springboot/xsisacademy/src/main/java/com/pembekalan/xsisacademy/repository/CategoryRepository.java
+++ Springboot/xsisacademy/src/main/java/com/pembekalan/xsisacademy/repository/CategoryRepository.java
@@ -1,11 +1,18 @@
 package com.pembekalan.xsisacademy.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.pembekalan.xsisacademy.entity.Category;
 
 public interface CategoryRepository extends JpaRepository<Category, Integer> {
-    
-    
+
+    // @Query(value = "select * from categories order by updated_at desc", nativeQuery = true)
+    // List<Category> getAllCategories();
+
+    @Query(value = "select c from Category c order by c.updatedAt desc")
+    List<Category> getAllCategories();
 
 }
---
+++ Springboot/xsisacademy/src/main/resources/static/js/category.js
@@ -0,0 +1,25 @@
+function openForm() {
+    $.ajax({
+        type: "get",
+        url: "/category/form",
+        contentType: "html",
+        success: function (categoryForm) {
+            $('#modalForm').modal('show');
+            $('.modal-title').html('Category Form');
+            $('.modal-body').html(categoryForm);
+        }
+    });
+}
+
+function editForm(id) {
+    $.ajax({
+        type: "get",
+        url: `/category/edit/${id}`,
+        contentType: "html",
+        success: function (categoryForm) {
+            $('#modalForm').modal('show');
+            $('.modal-title').html('Category Form');
+            $('.modal-body').html(categoryForm);
+        }
+    });
+}
